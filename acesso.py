68b931e37a9059d9df5100cf2d4bebd4be4153e3
Murilo Gomes <murilo.goomes@hotmail.com>
2022-07-16 17:53:59 -0300

    Adiciona triangulação para teste de acesso de hora cheia
    Co-authored-by: liversonp <liverson.p@gmail.com>
    Co-authored-by: Gabriela Pivetta <gpivetta99@gmail.com>

--- acesso.py
+++ acesso.py
@@ -1,16 +1,28 @@
 class Acesso:
     horaEntrada = ''
     horaSaida = ''
+    tipoEstacionamento = 0
 
-    def __init__(self, horaEntrada, horaSaida) -> None:
+    def __init__(self, horaEntrada, horaSaida, tipoEstacionamento) -> None:
         self.horaEntrada = horaEntrada
         self.horaSaida = horaSaida
+        self.tipoEstacionamento = tipoEstacionamento
 
     def getPrecoHoraCheia(self):
         horas = 60 * (int(self.horaSaida.split(':')[0]) - int(self.horaEntrada.split(':')[0]))
         minutos = (int(self.horaSaida.split(':')[1]) - int(self.horaEntrada.split(':')[1]))
         tempoTotal = horas + minutos
-        if(tempoTotal == 60):
-            return 102
+        if(self.tipoEstacionamento == 1):
+            f_valor = 30.00
+            v_hora = 15
+        
+        elif(self.tipoEstacionamento == 2):
+            f_valor = 20.00
+            v_hora = 10
+        
         else:
-            return 204
+            f_valor = 10.00
+            v_hora = 0
+        
+        valor_total = (4*f_valor*tempoTotal//60)
+        return valor_total - (v_hora/100) * valor_total
--- test_acesso_hora_cheia.py
+++ test_acesso_hora_cheia.py
@@ -2,11 +2,17 @@ from acesso import Acesso
 
 class TesteFuncionalAcessoHoraCheia():
     def testAcessoHoraCheia1(self):
-        acesso = Acesso('8:30', '9:30')
+        acesso = Acesso('8:30', '9:30', 1)
         assert acesso.getPrecoHoraCheia() == 102
 
     def testAcessoHoraCheia2(self):
-        acesso = Acesso('8:30', '9:30')
+        acesso = Acesso('8:30', '9:30', 1)
         assert acesso.getPrecoHoraCheia() == 102
-        acesso = Acesso('9:30', '11:30')
+        acesso = Acesso('9:30', '11:30', 1)
         assert acesso.getPrecoHoraCheia() == 204
+
+    def testAcessoHoraCheia3(self):
+        acesso = Acesso('8:20', '11:20', 2)
+        assert acesso.getPrecoHoraCheia() == 216
+        acesso = Acesso('12:00', '14:00', 3)
+        assert acesso.getPrecoHoraCheia() == 80
