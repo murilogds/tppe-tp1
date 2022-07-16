ca729bfc405aa661da95ac339176f7b8474f137b
Murilo Gomes <murilo.goomes@hotmail.com>
2022-07-16 17:34:59 -0300

    Adiciona duplicação para os testes de acesso de hora cheia
    Co-authored-by: liversonp <liverson.p@gmail.com>
    Co-authored-by: Gabriela Pivetta <gpivetta99@gmail.com>

--- acesso.py
+++ acesso.py
@@ -7,4 +7,10 @@ class Acesso:
         self.horaSaida = horaSaida
 
     def getPrecoHoraCheia(self):
-        return 102
+        horas = 60 * (int(self.horaSaida.split(':')[0]) - int(self.horaEntrada.split(':')[0]))
+        minutos = (int(self.horaSaida.split(':')[1]) - int(self.horaEntrada.split(':')[1]))
+        tempoTotal = horas + minutos
+        if(tempoTotal == 60):
+            return 102
+        else:
+            return 204
--- test_acesso_hora_cheia.py
+++ test_acesso_hora_cheia.py
@@ -1,6 +1,12 @@
 from acesso import Acesso
 
-class TestAcessoHoraCheia():
-    def testAcessoHoraCheiaEstacionamentoUm(self):
+class TesteFuncionalAcessoHoraCheia():
+    def testAcessoHoraCheia1(self):
         acesso = Acesso('8:30', '9:30')
         assert acesso.getPrecoHoraCheia() == 102
+
+    def testAcessoHoraCheia2(self):
+        acesso = Acesso('8:30', '9:30')
+        assert acesso.getPrecoHoraCheia() == 102
+        acesso = Acesso('9:30', '11:30')
+        assert acesso.getPrecoHoraCheia() == 204
