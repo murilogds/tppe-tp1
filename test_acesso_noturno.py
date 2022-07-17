b5cd228b34cd6a8c180d3a8f4b52cddc58711d9a
Murilo Gomes <murilo.goomes@hotmail.com>
2022-07-17 12:11:21 -0300

    Parametrização dos testes de acesso noturno
    Co-authored-by: Liverson Paulo Furtado Severo <liverson.p@gmail.com>
    Co-authored-by: Gabriela Pivetta <gpivetta99@gmail.com>

--- test_acesso_noturno.py
+++ test_acesso_noturno.py
@@ -2,29 +2,12 @@ import pytest
 from acesso import Acesso
 from estacionamento import Estacionamento
 
+@pytest.mark.parametrize("acesso, estacionamento, saida",[
+    (Acesso('21:00', '23:00', 'ABC123'), Estacionamento(diaria_noturna=54, entrada_noturna='19:00', retirada_noturna='8:00'), 54),
+    (Acesso('22:00', '04:00', 'ABC123'), Estacionamento(diaria_noturna=21, entrada_noturna='21:00', retirada_noturna='7:00'), 21),
+    (Acesso('01:00', '03:00', 'ABC123'), Estacionamento(diaria_noturna=20, entrada_noturna='20:00', retirada_noturna='8:00'), 20)
+])
 class TesteFuncionalAcessoNoturno():
-    
-    def testeAcessoNoturno1(self):
-        acesso = Acesso('21:00', '23:00', 'ABC123')
-        estacionamento = Estacionamento(diaria_noturna=54, entrada_noturna='19:00', retirada_noturna='8:00')
-        assert acesso.calculaAcesso(estacionamento) == 54
 
-    def testeAcessoNoturno2(self):
-        estacionamento1 = Estacionamento(diaria_noturna=54, entrada_noturna='19:00', retirada_noturna='8:00')
-        acesso1 = Acesso('21:00', '23:00', 'ABC123')
-        assert acesso1.calculaAcesso(estacionamento1) == 54
-        estacionamento2 = Estacionamento(diaria_noturna=21, entrada_noturna='21:00', retirada_noturna='7:00')
-        acesso2 = Acesso('22:00', '04:00', 'ABC123')
-        assert acesso2.calculaAcesso(estacionamento2) == 21
-
-    def testeAcessoNoturno3(self):
-        estacionamento1 = Estacionamento(diaria_noturna=54, entrada_noturna='19:00', retirada_noturna='8:00')
-        acesso1 = Acesso('21:00', '23:00', 'ABC123')
-        assert acesso1.calculaAcesso(estacionamento1) == 54
-        estacionamento2 = Estacionamento(diaria_noturna=21, entrada_noturna='21:00', retirada_noturna='7:00')
-        acesso2 = Acesso('22:00', '04:00', 'ABC123')
-        assert acesso2.calculaAcesso(estacionamento2) == 21
-        estacionamento3 = Estacionamento(diaria_noturna=20, entrada_noturna='20:00', retirada_noturna='8:00')
-        acesso3 = Acesso('01:00', '03:00', 'ABC123')
-        assert acesso3.calculaAcesso(estacionamento3) == 20
-        
+    def testeAcessoNoturno(self, acesso, estacionamento, saida):
+        assert acesso.calculaAcesso(estacionamento) == saida
